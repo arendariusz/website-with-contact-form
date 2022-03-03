54cc89eb16a5139b07fe8f161c0cee964761d637
arendariusz <d.arendarski92@gmail.com>
2022-03-03 21:14:54 +0100

    zadanie 22.0

---
+++ src/test/java/com/example/website_with_contact_form/WebsiteWithContactFormApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.website_with_contact_form;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsiteWithContactFormApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
